7cd02cec34482a7855bed07d377519a135871aea
zhanghang <zhanghang>
2020-07-14 17:20:53 +0800

    更新

--- src/main/java/cn/minsin/feign/config/FeignExceptionHandler.java
+++ src/main/java/cn/minsin/feign/config/FeignExceptionHandler.java
@@ -21,39 +21,39 @@ import java.util.Map;
 @Slf4j
 public class FeignExceptionHandler extends DefaultErrorAttributes {
 
-    @Override
-    public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
-        Throwable error = super.getError(webRequest);
-        //springMVC异常
-        if (error == null) {
-            return super.getErrorAttributes(webRequest, includeStackTrace);
-        }
-        Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
-        List<ExceptionChain> exceptionChains = null;
-        if (error instanceof RemoteCallException) {
-            exceptionChains = ((RemoteCallException) error).getExceptionChains();
-        } else {
-            Object attribute = webRequest.getAttribute(ExceptionConstant.EXCEPTION_CHAIN_KEY, RequestAttributes.SCOPE_REQUEST);
-            if (attribute != null) {
-                exceptionChains = JSON.parseArray(attribute.toString(), ExceptionChain.class);
-            }
-            if (exceptionChains == null) {
-                exceptionChains = new ArrayList<>(1);
-            }
-        }
+	@Override
+	public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+		Throwable error = super.getError(webRequest);
+		//springMVC异常
+		if (error == null) {
+			return super.getErrorAttributes(webRequest, includeStackTrace);
+		}
+		Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
+		List<ExceptionChain> exceptionChains = null;
+		if (error instanceof RemoteCallException) {
+			exceptionChains = ((RemoteCallException) error).getExceptionChains();
+		} else {
+			Object attribute = webRequest.getAttribute(ExceptionConstant.EXCEPTION_CHAIN_KEY, RequestAttributes.SCOPE_REQUEST);
+			if (attribute != null) {
+				exceptionChains = JSON.parseArray(attribute.toString(), ExceptionChain.class);
+			}
+		}
+		if (exceptionChains == null) {
+			exceptionChains = new ArrayList<>(1);
+		}
 
-        ExceptionChain exceptionChain = new ExceptionChain();
-        exceptionChain.setMessage(error.getMessage());
-        exceptionChain.setPath(errorAttributes.get("path").toString());
-        exceptionChain.setTimestamp(new Date());
-        exceptionChain.setApplicationName(FeignExceptionHandlerContext.getApplicationName());
-        //添加发生的异常类信息 以便下一步处理
-        if (error.getClass() != null) {
-            exceptionChain.setExceptionClass(error.getClass().getTypeName());
-        }
-        exceptionChains.add(exceptionChain);
-        errorAttributes.put(ExceptionConstant.EXCEPTION_CHAIN_KEY, exceptionChains);
-        return errorAttributes;
-    }
+		ExceptionChain exceptionChain = new ExceptionChain();
+		exceptionChain.setMessage(error.getMessage());
+		exceptionChain.setPath(errorAttributes.get("path").toString());
+		exceptionChain.setTimestamp(new Date());
+		exceptionChain.setApplicationName(FeignExceptionHandlerContext.getApplicationName());
+		//添加发生的异常类信息 以便下一步处理
+		if (error.getClass() != null) {
+			exceptionChain.setExceptionClass(error.getClass().getTypeName());
+		}
+		exceptionChains.add(exceptionChain);
+		errorAttributes.put(ExceptionConstant.EXCEPTION_CHAIN_KEY, exceptionChains);
+		return errorAttributes;
+	}
 }
 
